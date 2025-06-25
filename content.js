cbaa5e1cf25f468118f4a0b58ae4689fec698b47
Rushi <rushidani2006@gmail.com>
2025-06-25 21:36:56 +0530

    just successfully desplayed the bookmark icon to maang.in, leetcode and codeforces!

--- content.js
+++ content.js
@@ -1,18 +1,23 @@
+const platformClassMap = {
+  "maang.in": "d-flex align-items-center flex-wrap gap-2 justify-content-between mb-2",
+  "leetcode.com": "flex items-start justify-between gap-4",
+  "codeforces.com": "title",
+};
 const bookmarkIconURL = chrome.runtime.getURL("assets/bookmark.jpg");
 
+
 document.addEventListener("DOMContentLoaded", () => {
-  if (window.location.pathname.includes('/problems/')) {
+  console.log("\n\n\n\n drfgtvbyhnbgvfcdxfvgbhn \n\n\n");
+  if (window.location.pathname.includes("/problem")) {
     showMyBookMarkIcon();
   }
 });
 
-
 const observer = new MutationObserver(() => {
-  if (window.location.pathname.includes('/problems/')) {
-    const iconExists = document.querySelector('#my-bookmark-icon');
+  if (window.location.pathname.includes("/problem")) {
+    const iconExists = document.querySelector("#my-bookmark-icon");
     if (!iconExists) {
       showMyBookMarkIcon();
-      
     }
   }
 });
@@ -22,16 +27,15 @@ observer.observe(document.body, { childList: true, subtree: true });
 
 
 function showMyBookMarkIcon() {
-
-  const alreadyExists = document.querySelector('#my-bookmark-icon');
+  const alreadyExists = document.querySelector("#my-bookmark-icon");
   if (alreadyExists) return;
 
-  const aboveElement = document.getElementsByClassName(
-    "d-flex align-items-center flex-wrap gap-2 justify-content-between mb-2"
-  )[0];
+  const sectionWhereAddedButton = getClassNameAccordingPlatform();
+  if (!sectionWhereAddedButton) return;
 
+  const aboveElement = document.getElementsByClassName(sectionWhereAddedButton)[0];
   if (!aboveElement) {
-    console.warn("Reward button not found");
+    console.warn("Target DOM element not found for the given class.");
     return;
   }
 
@@ -44,55 +48,65 @@ function showMyBookMarkIcon() {
 
   aboveElement.parentNode.insertBefore(
     myBookMarkIcon,
-    aboveElement.nextSibling
+    aboveElement.nextSibling,
   );
 
   myBookMarkIcon.addEventListener("click", saveProblemForFuture);
 }
 
 
-
 async function saveProblemForFuture() {
   const problemUrl = window.location.href;
   const problemName = document.getElementsByClassName(
-    "coding_problem_info_heading__G9ueL fw-bolder rubik fs-4 mb-0"
+    "coding_problem_info_heading__G9ueL fw-bolder rubik fs-4 mb-0",
   )[0].textContent;
   const problemUniqueId = getProblemsUniqueId(problemUrl);
 
-  
   const result = await chrome.storage.sync.get(["bookmarkedProblems"]);
   const bookmarkedProblems = result.bookmarkedProblems || [];
-  
-  const isProblemAlreadyExist = bookmarkedProblems.some((problem) => problem.problemUniqueId.toLowerCase() === problemUniqueId.toLowerCase());
+
+  const isProblemAlreadyExist = bookmarkedProblems.some(
+    (problem) => problem.problemUniqueId.toLowerCase() === problemUniqueId.toLowerCase(),
+  );
   if (isProblemAlreadyExist) {
-      console.log("Problem already bookmarked.");
-      alert(`You have Already Added ${problemName} !`);
-      return;
-    }
-    
-    const newProblem = {
-        problemUrl: problemUrl,
-        problemName: problemName,
-        problemUniqueId: problemUniqueId,
-    };
-    
-    
-    console.log("Before push: ", bookmarkedProblems);
-    
-    bookmarkedProblems.push(newProblem);
-    
-    await chrome.storage.sync.set({ bookmarkedProblems });
-    
-    alert(`${problemName} bookmarked Successfully!`);
-    console.log("Bookmark updated:", bookmarkedProblems);
-    
+    console.log("Problem already bookmarked.");
+    alert(`You have Already Added ${problemName} !`);
+    return;
+  }
+
+  const newProblem = {
+    problemUrl: problemUrl,
+    problemName: problemName,
+    problemUniqueId: problemUniqueId,
+  };
+
+  console.log("Before push: ", bookmarkedProblems);
+
+  bookmarkedProblems.push(newProblem);
+
+  await chrome.storage.sync.set({ bookmarkedProblems });
+
+  alert(`${problemName} bookmarked Successfully!`);
+  console.log("Bookmark updated:", bookmarkedProblems);
 }
 
-
 function getProblemsUniqueId(url) {
-    const problemsPart = url.split("/problems/")[1];
-    if (!problemsPart) return null;
-    
-    const slug = problemsPart.split("?")[0];
-    return slug;
+  const problemsPart = url.split("/problems/")[1];
+  if (!problemsPart) return null;
+
+  const slug = problemsPart.split("?")[0];
+  return slug;
+}
+
+function getClassNameAccordingPlatform() {
+  const url = window.location.href;
+
+  for (const domain in platformClassMap) {
+    if (url.includes(domain)) {
+      return platformClassMap[domain];
+    }
+  }
+
+  console.warn("Platform not supported.");
+  return "";
 }
